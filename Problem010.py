f0ca484901afd7cd24c6e423ad414ce1f5aedcb2
Jeremy Gilreath <gilreathjw@gmail.com>
2019-09-28 12:42:35 -0400

    updated problem 10 with sieve of eratosthenes, massive speed improvement

--- Problem010.py
+++ Problem010.py
@@ -1,16 +1,20 @@
+import math
+
+
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 #
 # Find the sum of all the primes below two million.
 
 
 def sum_of_primes_below_n(n: int):
-    primes = [2]
-    i = 1
-    while i < n - 1:
-        i += 2
-        if all(i % y != 0 for y in primes):
-            primes.append(i)
-    return sum(primes)
+    not_primes = []
+    for x in range(2, int(math.sqrt(n)) + 1):
+        i = x
+        while i < n:
+            i += x
+            not_primes.append(i)
+    sieve = set(not_primes)
+    return sum([x for x in range(2, n) if x not in sieve])
 
 
 # def test():

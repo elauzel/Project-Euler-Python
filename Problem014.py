92c9aa05215758c7e7dcdd592e4f57b44f25d18e
Jeremy Gilreath <gilreathjw@gmail.com>
2019-10-01 11:14:34 -0400

    finished problem 14

--- Problem014.py
+++ Problem014.py
@@ -12,3 +12,38 @@
 # Which starting number, under one million, produces the longest chain?
 #
 # NOTE: Once the chain starts the terms are allowed to go above one million.
+
+
+def find_longest_collatz_chain(ceiling: int):
+    longest_chain = 0
+    longest_number = 0
+    for x in range(1, ceiling):
+        chain_length = _collatz_chain_length(x)
+        if chain_length > longest_chain:
+            longest_chain = chain_length
+            longest_number = x
+    return longest_number
+
+
+def _collatz_chain_length(n: int, chain_length: int = 1):
+    if n == 1:
+        return chain_length
+    elif n % 2 == 0:
+        return _collatz_chain_length(int(n / 2), chain_length + 1)
+    else:
+        return _collatz_chain_length(int(3 * n) + 1, chain_length + 1)
+
+
+# def test():
+#     assert 0 == find_longest_collatz_chain(1)
+#     assert 1 == find_longest_collatz_chain(2)
+#     assert 2 == find_longest_collatz_chain(3)
+#     assert 3 == find_longest_collatz_chain(4)
+#     assert 3 == find_longest_collatz_chain(5)
+#     assert 3 == find_longest_collatz_chain(6)
+#     assert 6 == find_longest_collatz_chain(7)
+#     assert 7 == find_longest_collatz_chain(8)
+#     assert 10 == _collatz_chain_length(13)
+
+
+print("answer:", find_longest_collatz_chain(1000000))

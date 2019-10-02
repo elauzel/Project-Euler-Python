a4047cf0dbfed64247ef7247cc1c8f965a67cbef
Jeremy Gilreath <gilreathjw@gmail.com>
2019-10-02 17:13:18 -0400

    finished problem 17

--- Problem016.py
+++ Problem016.py
@@ -3,27 +3,27 @@
 # What is the sum of the digits of the number 2^1000?
 
 
-def impl(exponent: int):
+def sum_of_digits_2_pow(exponent: int):
     return sum([int(x) for x in str(2 ** exponent)])
 
 
 # def test():
-#     assert 1 == impl(0)  # 1
-#     assert 2 == impl(1)  # 2
-#     assert 4 == impl(2)  # 4
-#     assert 8 == impl(3)  # 8
-#     assert 7 == impl(4)  # 16
-#     assert 5 == impl(5)  # 32
-#     assert 10 == impl(6)  # 64
-#     assert 11 == impl(7)  # 128
-#     assert 13 == impl(8)  # 256
-#     assert 8 == impl(9)  # 512
-#     assert 7 == impl(10)  # 1024
-#     assert 14 == impl(11)  # 2048
-#     assert 19 == impl(12)  # 4096
-#     assert 20 == impl(13)  # 8192
-#     assert 22 == impl(14)  # 16384
-#     assert 26 == impl(15)  # 32768
+#     assert 1 == sum_of_digits_2_pow(0)  # 1
+#     assert 2 == sum_of_digits_2_pow(1)  # 2
+#     assert 4 == sum_of_digits_2_pow(2)  # 4
+#     assert 8 == sum_of_digits_2_pow(3)  # 8
+#     assert 7 == sum_of_digits_2_pow(4)  # 16
+#     assert 5 == sum_of_digits_2_pow(5)  # 32
+#     assert 10 == sum_of_digits_2_pow(6)  # 64
+#     assert 11 == sum_of_digits_2_pow(7)  # 128
+#     assert 13 == sum_of_digits_2_pow(8)  # 256
+#     assert 8 == sum_of_digits_2_pow(9)  # 512
+#     assert 7 == sum_of_digits_2_pow(10)  # 1024
+#     assert 14 == sum_of_digits_2_pow(11)  # 2048
+#     assert 19 == sum_of_digits_2_pow(12)  # 4096
+#     assert 20 == sum_of_digits_2_pow(13)  # 8192
+#     assert 22 == sum_of_digits_2_pow(14)  # 16384
+#     assert 26 == sum_of_digits_2_pow(15)  # 32768
 
 
-print("answer:", impl(1000))
+print("answer:", sum_of_digits_2_pow(1000))
--- Problem017.py
+++ Problem017.py
@@ -6,3 +6,112 @@
 # NOTE: Do not count spaces or hyphens.
 # e.g. 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
 # The use of "and" when writing out numbers is in compliance with British usage.
+
+_ones_place_numbers = {
+    '1': 3,  # one
+    '2': 3,  # two
+    '3': 5,  # three
+    '4': 4,  # four
+    '5': 4,  # five
+    '6': 3,  # six
+    '7': 5,  # seven
+    '8': 5,  # eight
+    '9': 4,  # nine
+}
+_tens_place_numbers = {
+    '2': 6,  # twenty
+    '3': 6,  # thirty
+    '4': 5,  # forty
+    '5': 5,  # fifty
+    '6': 5,  # sixty
+    '7': 7,  # seventy
+    '8': 6,  # eighty
+    '9': 6,  # ninety
+}
+_other_numbers = {
+    '10': 3,  # ten
+    '11': 6,  # eleven
+    '12': 6,  # twelve
+    '13': 8,  # thirteen
+    '14': 8,  # fourteen
+    '15': 7,  # fifteen
+    '16': 7,  # sixteen
+    '17': 9,  # seventeen
+    '18': 8,  # eighteen
+    '19': 8,  # nineteen
+}
+
+
+def impl(ceiling: int):
+    sum_ = 0
+    for x in range(1, ceiling):
+        if x > 999:
+            sum_ += _sum_four_digits(x)
+        elif x > 99:
+            sum_ += _sum_three_digits(x)
+        elif x > 9:
+            sum_ += _sum_two_digits(x)
+        elif x > 0:
+            sum_ += _ones_place_numbers.get(str(x))
+    return sum_
+
+
+def _sum_four_digits(n: int):
+    s = str(n)
+    sum_ = 0
+    if 999 < n < 10000:
+        sum_ += _ones_place_numbers.get(s[0])
+        sum_ += 8  # thousand
+        temp = n % 1000
+        if temp != 0:
+            if temp > 99:
+                sum_ += _sum_three_digits(temp)
+            else:
+                sum_ += 3  # and
+                if temp > 9:
+                    sum_ += _sum_two_digits(temp)
+                elif temp > 0:
+                    sum_ += _ones_place_numbers.get(str(temp))
+    return sum_
+
+
+def _sum_three_digits(n: int):
+    s = str(n)
+    sum_ = 0
+    if 99 < n < 1000:
+        sum_ += _ones_place_numbers.get(s[0])
+        sum_ += 7  # hundred
+        temp = n % 100
+        if temp != 0:
+            sum_ += 3  # and
+            if temp > 9:
+                sum_ += _sum_two_digits(temp)
+            else:
+                sum_ += _ones_place_numbers.get(str(temp))
+    return sum_
+
+
+def _sum_two_digits(n: int):
+    s = str(n)
+    sum_ = 0
+    if 9 < n < 100:
+        if n > 19:
+            sum_ += _tens_place_numbers.get(s[0])
+            if n % 10 != 0:
+                sum_ += _ones_place_numbers.get(s[1])
+        else:
+            sum_ += _other_numbers.get(s)
+    return sum_
+
+
+# def test():
+#     assert 19 == impl(6)  # one two three four five
+#     assert 6 == _sum_two_digits(20)  # twenty
+#     assert 10 == _sum_three_digits(100)  # one hundred
+#     assert 16 == _sum_three_digits(101)  # one hundred and one
+#     assert 20 == _sum_three_digits(115)  # one hundred and fifteen
+#     assert 24 == _sum_three_digits(197)  # one hundred and ninety seven
+#     assert 11 == _sum_four_digits(1000)  # one thousand
+#     assert 17 == _sum_four_digits(1001)  # one thousand and one
+
+print("answer:", impl(1001))
